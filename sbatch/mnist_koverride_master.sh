e3ff760f52ea9ac0e291f751e9d0bf958ed0ba85
sdmuhsin <sdmuhsin@gmail.com>
2024-11-21 09:59:29 -0600

    k override master sbatch script for mnist

---
+++ sbatch/mnist_koverride_master.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Beginning k override sbatch scripts for MNIST"
+
+# Iterate over k_override values from 0.1 to 2.0
+for k in $(seq 0.1 0.1 0.2)
+do
+    echo "Submitting job with k_override = $k"
+    sbatch --wrap "sh sbatch/sbatch_base.sh && ipython3 src/Experiments/experiment_pTTQ_experimental.py --parameters_file=./parameters_files/MNIST/mnist_experimental.json --k_override=$k"
+done
+
+echo "All jobs submitted"
