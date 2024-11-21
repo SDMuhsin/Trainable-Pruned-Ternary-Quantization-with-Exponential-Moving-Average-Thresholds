480acf315624887404d676d0a1bf215585a521d1
sdmuhsin <sdmuhsin@gmail.com>
2024-11-21 10:00:23 -0600

    Syntax err

--- sbatch/mnist_koverride_master.sh
+++ sbatch/mnist_koverride_master.sh
@@ -6,7 +6,7 @@ echo "Beginning k override sbatch scripts for MNIST"
 for k in $(seq 0.1 0.1 0.2)
 do
     echo "Submitting job with k_override = $k"
-    sbatch --wrap "sh sbatch/sbatch_base.sh && ipython3 src/Experiments/experiment_pTTQ_experimental.py --parameters_file=./parameters_files/MNIST/mnist_experimental.json --k_override=$k"
+    sbatch --wrap "sh sbatch/sbatch_base.sh && python3 src/Experiments/experiment_pTTQ_experimental.py --parameters_file=./parameters_files/MNIST/mnist_experimental.json --k_override=$k"
 done
 
 echo "All jobs submitted"
