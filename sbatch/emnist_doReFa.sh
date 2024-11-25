782d802b6a889375f30d0e42dee10a3a77d51343
sdmuhsin <sdmuhsin@gmail.com>
2024-11-25 08:52:30 -0600

    Increase mem for emnist

--- sbatch/emnist_doReFa.sh
+++ sbatch/emnist_doReFa.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus=1           # Allocate all GPUs to the job
-#SBATCH --mem=3000M
+#SBATCH --mem=5000M
 #SBATCH --time=1-00:00
 #SBATCH --chdir=/scratch/sdmuhsin/Trainable-Pruned-Ternary-Quantization-with-Exponential-Moving-Average-Thresholds
 #SBATCH --output=emnist-doReFa-%N-%j.out
