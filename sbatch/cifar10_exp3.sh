871ef0b966663a63e8eb0eaae1e55ff98fcb17b4
sdmuhsin <sdmuhsin@gmail.com>
2024-12-01 17:13:11 -0600

    Spacing issues exp3 scripts

--- sbatch/cifar10_exp3.sh
+++ sbatch/cifar10_exp3.sh
@@ -3,22 +3,22 @@
 echo "Beginning k override sbatch scripts for CIFAR10"
 
 sbatch \
---nodes=1 \
---ntasks-per-node=1 \
---cpus-per-task=1 \
---gpus=1 \
---mem=8000M \
---time=3-00:00 \
---chdir=/scratch/sdmuhsin/Trainable-Pruned-Ternary-Quantization-with-Exponential-Moving-Average-Thresholds \
---output=cifar10-exp3-%N-%j.out \
---wrap="
-    module load python/3.10
-    module load arrow/16.1.0
-    source ./env/bin/activate
-    echo 'Environment loaded'
-    which python3
-    export PYTHONPATH=\"\$PYTHONPATH:\$(pwd)\"
-    python3 src/Experiments/experiment_TTQ.py --parameters_file=./parameters_files/CIFAR10/cifar10_exp3.json
-"
+	--nodes=1 \
+	--ntasks-per-node=1 \
+	--cpus-per-task=1 \
+	--gpus=1 \
+	--mem=8000M \
+	--time=3-00:00 \
+	--chdir=/scratch/sdmuhsin/Trainable-Pruned-Ternary-Quantization-with-Exponential-Moving-Average-Thresholds \
+	--output=cifar10-exp3-%N-%j.out \
+	--wrap="
+	    module load python/3.10
+	    module load arrow/16.1.0
+	    source ./env/bin/activate
+	    echo 'Environment loaded'
+	    which python3
+	    export PYTHONPATH=\"\$PYTHONPATH:\$(pwd)\"
+	    python3 src/Experiments/experiment_TTQ.py --parameters_file=./parameters_files/CIFAR10/cifar10_exp3.json
+	"
 
 echo "All jobs submitted"
--- sbatch/mnist_exp3.sh
+++ sbatch/mnist_exp3.sh
@@ -3,22 +3,22 @@
 echo "Beginning k override sbatch scripts for MNIST"
 
 sbatch \
---nodes=1 \
---ntasks-per-node=1 \
---cpus-per-task=1 \
---gpus=1 \
---mem=6000M \
---time=2-00:00 \
---chdir=/scratch/sdmuhsin/Trainable-Pruned-Ternary-Quantization-with-Exponential-Moving-Average-Thresholds \
---output=mnist-exp3-%N-%j.out \
---wrap="
-    module load python/3.10
-    module load arrow/16.1.0
-    source ./env/bin/activate
-    echo 'Environment loaded'
-    which python3
-    export PYTHONPATH=\"\$PYTHONPATH:\$(pwd)\"
-    python3 src/Experiments/experiment_TTQ.py --parameters_file=./parameters_files/MNIST/mnist_exp3.json
-"
+	--nodes=1 \
+	--ntasks-per-node=1 \
+	--cpus-per-task=1 \
+	--gpus=1 \
+	--mem=6000M \
+	--time=2-00:00 \
+	--chdir=/scratch/sdmuhsin/Trainable-Pruned-Ternary-Quantization-with-Exponential-Moving-Average-Thresholds \
+	--output=mnist-exp3-%N-%j.out \
+	--wrap="
+	    module load python/3.10
+	    module load arrow/16.1.0
+	    source ./env/bin/activate
+	    echo 'Environment loaded'
+	    which python3
+	    export PYTHONPATH=\"\$PYTHONPATH:\$(pwd)\"
+	    python3 src/Experiments/experiment_TTQ.py --parameters_file=./parameters_files/MNIST/mnist_exp3.json
+	"
 
 echo "All jobs submitted"
